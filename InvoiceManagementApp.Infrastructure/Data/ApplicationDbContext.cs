db08bc13adc9eca0382b0886ecfb28e202f9d51b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-21 23:03:27 +0300

    Create ConfigureInfrastructureServices class

--- InvoiceManagementApp.Infrastructure/Data/ApplicationDbContext.cs
+++ InvoiceManagementApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IdentityServer4.EntityFramework.Options;
+using InvoiceManagementApp.Infrastructure.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 namespace InvoiceManagementApp.Infrastructure.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(
+           DbContextOptions options,
+           IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
     }
 }
--- InvoiceManagementApp.Infrastructure/StartUp.cs
+++ InvoiceManagementApp.Infrastructure/StartUp.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using InvoiceManagementApp.Infrastructure.Data;
+using InvoiceManagementApp.Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -10,8 +14,23 @@ namespace InvoiceManagementApp.Infrastructure
 {
     public static class StartUp
     {
-        public static IServiceCollection ServiceCollection(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
             return services;
         }
     }
