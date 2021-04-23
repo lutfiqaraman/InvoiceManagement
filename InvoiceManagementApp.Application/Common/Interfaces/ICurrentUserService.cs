2445dd536c85f7cc50bddd9aceb0284e1075ce11
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-23 23:06:53 +0300

    remove set form UserId properties

--- InvoiceManagementApp.Application/Common/Interfaces/ICurrentUserService.cs
+++ InvoiceManagementApp.Application/Common/Interfaces/ICurrentUserService.cs
@@ -8,6 +8,6 @@ namespace InvoiceManagementApp.Application.Common.Interfaces
 {
     public interface ICurrentUserService
     {
-        string UserId { get; set; }
+        string UserId { get; }
     }
 }
