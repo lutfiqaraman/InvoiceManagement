6161bad8139a13c848f27b6da696388ca0573df4
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-26 18:22:32 +0300

    Create CQRS approach

---
+++ InvoiceManagementApp.Application/Invoices/Handlers/CreateInvoiceCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using InvoiceManagementApp.Application.Common.Interfaces;
+using InvoiceManagementApp.Application.Invoices.Commands;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementApp.Application.Invoices.Handlers
+{
+    public class CreateInvoiceCommandHandler : IRequestHandler<CreateInvoiceCommand, int>
+    {
+        private readonly IApplicationDbContext Context;
+        public CreateInvoiceCommandHandler(IApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public Task<int> Handle(CreateInvoiceCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
