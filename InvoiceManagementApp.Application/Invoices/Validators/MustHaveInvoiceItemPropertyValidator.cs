d6e20934af8fedc3964d260d1c52299643256eec
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-28 23:34:26 +0300

    Create CreateInvoiceCommandValidator class

---
+++ InvoiceManagementApp.Application/Invoices/Validators/MustHaveInvoiceItemPropertyValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Validators;
+using InvoiceManagementApp.Application.Invoices.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InvoiceManagementApp.Application.Invoices.Validators
+{
+    public class MustHaveInvoiceItemPropertyValidator : PropertyValidator
+    {
+        protected override bool IsValid(PropertyValidatorContext context)
+        {
+            var list = context.PropertyValue as IList<InvoiceItemViewModel>;
+            return list != null && list.Any();
+        }
+    }
+}
