123954b9c45c1075e4277e214da66b92f97b4dd2
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-30 05:50:31 +0300

    Create API base controller

---
+++ InvoiceManagement.API/Controllers/ApiController.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceManagementApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class ApiController : ControllerBase
+    {
+        private IMediator mediator;
+        protected IMediator Mediator => mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
