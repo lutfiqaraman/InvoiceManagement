a61bd2212c086521f7388b0bec907fa2a2d185d8
lutfiqaraman <lutfiqaraman@gmail.com>
2021-04-30 05:56:33 +0300

    Create Invoice Controller

---
+++ InvoiceManagement.API/Controllers/InvoicesController.cs
@@ -0,0 +1,21 @@
+﻿using InvoiceManagementApp.Application.Invoices.Commands;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementApp.API.Controllers
+{
+    [Authorize]
+    public class InvoicesController : ApiController
+    {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create(CreateInvoiceCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
