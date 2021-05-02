b2fa2dae180fec08037a6d3f2a2b52796939acd4
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-02 18:13:25 +0300

    create a GetAllInvoices Method

--- InvoiceManagement.API/Controllers/InvoicesController.cs
+++ InvoiceManagement.API/Controllers/InvoicesController.cs
@@ -1,6 +1,10 @@
-﻿using InvoiceManagementApp.Application.Invoices.Commands;
+﻿using InvoiceManagementApp.Application.Common.Interfaces;
+using InvoiceManagementApp.Application.Invoices.Commands;
+using InvoiceManagementApp.Application.Invoices.Queries;
+using InvoiceManagementApp.Application.Invoices.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace InvoiceManagementApp.API.Controllers
@@ -8,9 +12,26 @@ namespace InvoiceManagementApp.API.Controllers
     [Authorize]
     public class InvoicesController : ApiController
     {
-        public async Task<ActionResult<int>> Create(CreateInvoiceCommand command)
+        private readonly ICurrentUserService CurrentUserService;
+
+        public InvoicesController(ICurrentUserService currentUserService)
+        {
+            CurrentUserService = currentUserService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> CreateInvoice(CreateInvoiceCommand command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpGet]
+        public async Task<IList<InvoiceViewModel>> GetAllInvoices()
+        {
+            return await Mediator.Send(new GetUserInvoicesQuery
+            {
+                User = CurrentUserService.UserId
+            });
         }
     }
 }
