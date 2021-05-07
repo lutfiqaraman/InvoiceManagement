061d691ea80f16af5bdb16495ac7c988a55cac2b
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-07 15:41:21 +0300

    Create the automapper class

---
+++ InvoiceManagementApp.Application/Invoices/MappingProfiles/InvoiceMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using InvoiceManagementApp.Application.Invoices.Commands;
+using InvoiceManagementApp.Application.Invoices.ViewModels;
+using InvoiceManagementApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementApp.Application.Invoices.MappingProfiles
+{
+    public class InvoiceMappingProfile : Profile
+    {
+        public InvoiceMappingProfile()
+        {
+            CreateMap<Invoice, InvoiceViewModel>();
+            CreateMap<InvoiceViewModel, Invoice>();
+
+            CreateMap<InvoiceItemViewModel, InvoiceItem>();
+            CreateMap<InvoiceItem, InvoiceItemViewModel>().ConstructUsing(i => new InvoiceItemViewModel
+            {
+                Id   = i.Id,
+                Item = i.Item,
+                Qty  = i.Qty,
+                Rate = i.Rate
+            });
+
+            CreateMap<CreateInvoiceCommand, Invoice>();
+        }
+    }
+}
--- InvoiceManagementApp.Application/Invoices/Validators/MustHaveInvoiceItemPropertyValidator.cs
+++ InvoiceManagementApp.Application/Invoices/Validators/MustHaveInvoiceItemPropertyValidator.cs
@@ -5,11 +5,12 @@ using System.Linq;
 
 namespace InvoiceManagementApp.Application.Invoices.Validators
 {
+    [System.Obsolete]
     public class MustHaveInvoiceItemPropertyValidator : PropertyValidator
     {
         protected override bool IsValid(PropertyValidatorContext context)
         {
-            var list = context.PropertyValue as IList<InvoiceItemViewModel>;
+            IList<InvoiceItemViewModel> list = context.PropertyValue as IList<InvoiceItemViewModel>;
             return list != null && list.Any();
         }
     }
