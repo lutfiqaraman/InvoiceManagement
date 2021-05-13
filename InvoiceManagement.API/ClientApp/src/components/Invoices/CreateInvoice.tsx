3ea445ed931233b9a8445273c99cf99f3e6638d8
lutfiqaraman <lutfiqaraman@gmail.com>
2021-05-13 23:39:09 +0300

    create a form

--- InvoiceManagement.API/ClientApp/src/components/Invoices/CreateInvoice.tsx
+++ InvoiceManagement.API/ClientApp/src/components/Invoices/CreateInvoice.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Card, CardBody, Row, Col, Input, FormGroup } from 'reactstrap';
 
 interface ICreateInvoice {
 
@@ -6,8 +7,21 @@ interface ICreateInvoice {
 
 const CreateInvoice : React.FC<ICreateInvoice> = ({ }) => {
     return (
-        <div>
-            <h1>Create Invoice</h1>
+        <div className="col-md-12">
+            <Card>
+                <CardBody>
+                    <Row>
+                        <Col>
+                            <FormGroup>
+                                <Input type="text" placeholder="From whom" />
+                            </FormGroup>
+                            <FormGroup>
+                                <Input type="text" placeholder="To whom" />
+                            </FormGroup>
+                        </Col>
+                    </Row>
+                </CardBody>
+            </Card>
         </div>
     );
 }
